871e932a2b71a9ca2da2b9869f777c630a4be125
AnnaHort <annahort3d@gmail.com>
2025-02-22 13:44:49 +0200

    swiper settings

---
+++ js/swiper.js
@@ -0,0 +1,43 @@
+const swiperTimeless = new Swiper('.swiper-timeless', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3, 
+    centeredSlides: true, // Центрує активний слайд
+    spaceBetween: 16, // Відстань між слайдами
+    
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+      });
+
+
+const swiperBrands = new Swiper('.swiper-brands', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 2, 
+    spaceBetween: 16, // Відстань між слайдами
+  
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+      // автопрокрутка
+    autoplay: {
+  // пауза між прокруткою
+      delay: 1000,
+      // закінчити на останньому слайді 
+      stopOnLastSlide: true,
+      // відключити після ручного переключення
+      disableOnInteraction: false
+    },
+  
+    // швидікість автопрокруту 
+    speed: 3000,
+  });
+  
