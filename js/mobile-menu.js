2ad4a2496a3f223c17924a2d545092faffdf86e3
AnnaHort <annahort3d@gmail.com>
2025-04-08 21:54:14 +0300

    close mobile modal ESC & overlay

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -6,5 +6,21 @@ function toggleMenu() {
     mobileMenu.classList.toggle('overlay--active');
     }
 
+function closeEscape(event) {
+    if (event.key === 'Escape') {
+        mobileMenu.classList.remove('overlay--active');
+    }
+}
+
+function closeClick() {
+    mobileMenu.classList.remove('overlay--active');
+}
+
+document.addEventListener('click', (event) => {
+    if (event.target === mobileMenu) {
+        closeClick();
+    }
+});
+document.addEventListener('keydown', closeEscape);
 burgerButton.addEventListener('click', toggleMenu);
 closeButton.addEventListener('click', toggleMenu);
